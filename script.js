fabddef5f451a4be60ffdd8ffe0860d3908ff52d
rodrigotfcode <52326702+rodrigotfcode@users.noreply.github.com>
2023-07-25 23:59:39 -0300

    Manipulando estilos CSS via DOM

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelector('.guess').value = 15;
 
 const secretNumber = Math.trunc(Math.random() * 15) + 1;
 let score = 20;
-
+console.log(secretNumber)
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -19,23 +19,32 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '👀 No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✔️ Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = guess;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '✨ Too high!';
       score--;
       document.querySelector('.score').textContent = score;
+      document.querySelector('body').style.backgroundColor = '#F2CB05';
+      
     } else {
       document.querySelector('.message').textContent = '❗ You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#C92724'
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🛎️ Too low!';
       score--;
       document.querySelector('.score').textContent = score;
+      document.querySelector('body').style.backgroundColor = '#F2CB05'
     } else {
       document.querySelector('.message').textContent = '❗ You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#C92724'
     }
     
   }
